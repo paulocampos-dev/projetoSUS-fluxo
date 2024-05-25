348210a77a6cb5698e58aa2aff21a78446791d4a
Thiago-N-S <thiagoneres202@gmail.com>
2024-05-25 18:07:53 -0300

    Main inicio da parte de informações, assim como esta no esquema do fluxo

---
+++ features/Info.js
@@ -0,0 +1,19 @@
+module.exports = function (controller) {
+
+    const { BotkitConversation } = require("botkit");
+    const flow = new BotkitConversation("info", controller);
+    // const nlu = require('../scripts/nlu.js');  flow.after(async (response, bot) => {
+    
+    flow.addAction("info") 
+
+    flow.addMessage(JSON.stringify({
+        "type":"message",
+        "section": "informação",
+        "body": "que tipo de informação você gostaria de ter ?"
+    }), "info")
+
+    flow.after(async (response, bot) => {
+        await bot.cancelAllDialogs();
+    });
+    controller.addDialog(flow);
+};
